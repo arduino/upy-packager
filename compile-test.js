26aa5384887d26b61ae34546f56bf743f34ecb9b
Sebastian Romero <s.romero@arduino.cc>
2024-09-12 15:03:47 +0200

    Remove unused code

--- compile-test.js
+++ compile-test.js
@@ -1,11 +1,6 @@
 import { MPyCrossCompiler } from "./logic/mpy-cross-compiler.js";
-import MicroPythonBoard from "micropython.js";
 
-const DEFAULT_PORT = '/dev/cu.usbmodem1234561'
-const board = new MicroPythonBoard()
-await board.open(DEFAULT_PORT)
-
-const compiler = new MPyCrossCompiler(board);
+const compiler = new MPyCrossCompiler();
 const sourceFiles = [
     '/Users/sebastian/Downloads/radio_control/__init__.py',
     '/Users/sebastian/Downloads/radio_control/transmitter.py'
