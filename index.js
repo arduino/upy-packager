cc6c094640cb0be6bcd7dbf3d74a43dd913ac42c
Sebastian Romero <s.romero@arduino.cc>
2024-10-02 11:54:47 +0200

    Fix broken exports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { MPyCrossCompiler } from './logic/MPyCrossCompiler.js';
-import { PackageInstaller } from './logic/PackageInstaller.js';
-import { RepositoryArchiver, ArchiveResult } from './logic/RepositoryArchiver.js';
-import { Packager } from './logic/Packager.js';
+import { MPyCrossCompiler } from './logic/mpy-cross-compiler.js';
+import { PackageInstaller } from './logic/package-installer.js';
+import { RepositoryArchiver, ArchiveResult } from './logic/repository-archiver.js';
+import { Packager } from './logic/packager.js';
 
 export { MPyCrossCompiler, PackageInstaller, RepositoryArchiver, ArchiveResult, Packager };
